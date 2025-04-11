89d2d0f4bf79a7629e52e2e3598c524cafb0bdae
Gow2123 <grs231202@gmail.com>
2025-04-11 15:41:59 +0530

    Simplify configuration for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
-      }
-    }
+    outDir: 'dist'
   },
   server: {
     port: 3000,

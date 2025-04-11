57407e277a3d1bca5b48e29707d31f73c63ab79e
Gow2123 <grs231202@gmail.com>
2025-04-11 15:45:16 +0530

    setup api key

--- src/services/geminiService.js
+++ src/services/geminiService.js
@@ -1,5 +1,5 @@
 const GEMINI_API_KEY = 'AIzaSyBkQcoIb4W3XjSGG9a5Uw55t2bZLMSUpDo';
-const GEMINI_API_URL = 'https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent';
+const GEMINI_API_URL = 'https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent';
 
 export const sendMessageToGemini = async (message) => {
   try {
